ecc81dc161b80aebe8fc96e2f097a64e8f5f0fcf
chalottev <chalotte_villadsen@hotmail.com>
2013-09-09 20:36:05 +0200

    Start

---
+++ TestHjemmeside/Controllers/AddressController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestHjemmeside.Models;
+
+namespace TestHjemmeside.Controllers
+{
+    public class AddressController : Controller
+    {
+        //
+        // GET: /Address/
+
+        public ActionResult Index()
+        {
+            var liste = new List<Adresse>();
+            liste.Add(new Adresse
+            {
+                Name = "Chalotte",
+                Vej = "Århusvej",
+            });
+            liste.Add(new Adresse {
+                Name = "Christian",
+                Vej = "Månen",
+            });
+
+            return View(liste);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestHjemmeside/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestHjemmeside.MvcApplication" Language="C#" %>
